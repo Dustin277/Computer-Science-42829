189100d5bf3ed69214f88391dff83f86679403ec
Dustin277 <185316dus>
2016-03-02 07:42:33 -0800

    Signed-off-by: Dustin277 <185316dus>

---
+++ Hello World/main.cpp
@@ -0,0 +1,34 @@
+/* 
+ * File:   main.cpp
+ * Author: Dustin Badillo
+ * Created on February 17, 2016, 9:45 AM
+ * Purpose: Our first program
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants no in-constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare variables no doubles
+    
+    //Initialize variables
+    
+    //Input values
+    
+    //Map the input to the outputs
+    
+    //Output the results
+    cout<<"Hello World"<<endl;
+    
+    //Exit Stage Right!
+    return 0;
+}
+
